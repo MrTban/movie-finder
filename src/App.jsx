bb82b5b57069935ef0698dba13fd41f9d2cc8df5
MrTban <esteman214@gmail.com>
2023-05-15 10:25:00 -0500

    resnder movies response and use hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
 import './App.css'
 
+import { useMovies } from './hooks/useMovies'
+
+import { Movies } from './components/Movies'
+
 function App() {
+	const { movies } = useMovies()
 	return (
-		<div>
+		<div className='flex flex-col justify-center items-center'>
 			<header>
-				<h1>Movie Finder</h1>
-				<form className='form'>
+				<h1 className='text-center'>Movie finder</h1>
+				<form className='flex items-center justify-center'>
 					<input placeholder='Avengers, Star Wars, Fast & Furious...' />
 					<button type='submit'>Search</button>
 				</form>
 			</header>
 
-			<main>Aquí irán los resultados</main>
+			<main className='flex justify-center'>
+				<Movies movies={movies} />
+			</main>
 		</div>
 	)
 }

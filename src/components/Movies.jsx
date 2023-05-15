3b840c67a547f26d5103d7dc8c28358b4446d5ba
MrTban <esteman214@gmail.com>
2023-05-15 10:17:25 -0500

    render

---
+++ src/components/Movies.jsx
@@ -0,0 +1,23 @@
+const ListOfMovies = ({ movies }) => {
+	return (
+		<ul>
+			{movies.map((movie) => (
+				<li key={movie.id}>
+					<h3>{movie.title}</h3>
+					<h3>{movie.year}</h3>
+					<img src={movie.poster} alt={movie.title} />
+				</li>
+			))}
+		</ul>
+	)
+}
+
+const NoMoviesResults = () => {
+	return <p>No movies found</p>
+}
+
+export const Movies = ({ movies }) => {
+	const hasMovies = movies?.length > 0
+
+	return hasMovies ? <ListOfMovies movies={movies} /> : <NoMoviesResults />
+}
